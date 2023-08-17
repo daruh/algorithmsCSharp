d6ee1f06decce36385b76ba526968c041c31f812
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-08-17 14:51:14 +0200

    directed dfs impl+tests

---
+++ AlgorithmsCSharp/Graphs/Directed/Searches/DirectedDfs.cs
@@ -0,0 +1,43 @@
+﻿
+namespace AlgorithmsCSharp.Graphs.Directed.Searches
+{
+    public class DirectedDfs
+    {
+        private bool[] marked;
+
+        public DirectedDfs(IDigraph g, int s)
+        {
+            marked = new bool[g.V()];
+            dfs(g, s);
+        }
+
+        public DirectedDfs(IDigraph g, IEnumerable<int> sources)
+        {
+            marked = new bool[g.V()];
+            foreach (var s in sources)
+            {
+                if (!marked[s])
+                {
+                    dfs(g, s);
+                }
+            }
+        }
+
+        private void dfs(IDigraph g, int v)
+        {
+            marked[v] = true;
+            foreach (var w in g.Adjacent(v))
+            {
+                if (!marked[w])
+                {
+                    dfs(g, w);
+                }
+            }
+        }
+
+        public bool IsMarked(int w)
+        {
+            return marked[w];
+        }
+    }
+}
---
+++ algorithmsCSharpTests/DigraphsTests/DirectedDFSTests.cs
@@ -0,0 +1,34 @@
+﻿using AlgorithmsCSharp.Graphs.Directed.Searches;
+using AlgorithmsCSharp.Graphs.Undirected.Readers;
+using AlgorithmsCSharp.Graphs.Undirected.Searches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithmsCSharpTests.DigraphsTests
+{
+    [TestClass]
+    public class DepthFirstSearchTests
+    {
+        [TestMethod]
+        public void TestSimpleDepthFirstSearch()
+        {
+            var fileRead = new FileDigraphReader();
+            var graph = fileRead.ReadFromFile("DigraphsData\\tinyDG.dat");
+            var dfs = new DirectedDfs(graph, 0);
+
+            for (int i = 0; i < 6; i++)
+            {
+                Assert.IsTrue(dfs.IsMarked(i));
+            }
+
+            for (int i = 6; i < graph.V(); i++)
+            {
+                Assert.IsFalse(dfs.IsMarked(i));
+            }
+
+        }
+    }
+}
