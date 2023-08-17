5f289f36357228bb9b4bf3d67c81dc8dba24009f
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-08-17 14:12:19 +0200

    dfs tracing

---
+++ AlgorithmsCSharp/Graphs/Searches/DepthFirstSearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCSharp.Graphs.Searches
+{
+    public class DepthFirstSearch
+    {
+        private bool[] marked;
+        private int count;
+
+        public DepthFirstSearch(IGraph g, int s)
+        {
+            marked = new bool[g.V()];
+            dfs(g,s);
+        }
+
+        private void dfs(IGraph g, int v)
+        {
+            marked[v] = true;
+            count++;
+            foreach (var w in g.Adjacent(v))
+            {
+                if (!marked[w])
+                {
+                    dfs(g, w);
+                }
+            }
+        }
+
+        public bool IsMarked(int w)
+        {
+            return marked[w];
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+    }
+}
---
+++ algorithmsCSharpTests/GraphsTests/DepthFirstSearchTests.cs
@@ -0,0 +1,23 @@
+﻿using AlgorithmsCSharp.Graphs.Readers;
+using AlgorithmsCSharp.Graphs.Searches;
+
+namespace algorithmsCSharpTests.GraphsTests
+{
+    [TestClass]
+    public class DepthFirstSearchTests
+    {
+        [TestMethod]
+        public void TestSimpleDepthFirstSearch()
+        {
+            var fileRead = new FileGraphReader();
+            var graph = fileRead.ReadFromFile("GraphsData\\tinyCG.dat");
+            var dfs = new DepthFirstSearch(graph, 0);
+            Assert.AreEqual(6, dfs.Count());
+            for (int i = 0; i < 6; i++)
+            {
+                Assert.IsTrue(dfs.IsMarked(i)); 
+            }
+           
+        }
+    }
+}
