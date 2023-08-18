99f438ba28d41538002145dd0275e3810e6c35f5
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-08-18 08:51:39 +0200

    NFA- state machine build and recoginze

---
+++ AlgorithmsCSharp/RegularExpressions/NFA.cs
@@ -0,0 +1,99 @@
+﻿using AlgorithmsCSharp.Graphs.Directed;
+using AlgorithmsCSharp.Graphs.Directed.Searches;
+
+namespace AlgorithmsCSharp.RegularExpressions
+{
+    public class Nfa
+    {
+        private char[] re;
+        private Digraph G;
+        private int M;
+
+        public Nfa(string regexp)
+        {
+            var ops = new Stack<int>();
+            re = regexp.ToCharArray();
+            M = re.Length;
+            G = new Digraph(M + 1);
+
+
+            for (var i = 0; i < M; i++)
+            {
+                var lp = i;
+                if (re[i] == '(' || re[i] == '|')
+                {
+                    ops.Push(i);
+                }
+                else if (re[i] == ')')
+                {
+                    var or = ops.Pop();
+                    if (re[or] == '|')
+                    {
+                        lp = ops.Pop();
+                        G.AddEdge(lp, or + 1);
+                        G.AddEdge(or, i);
+                    }
+                    else
+                    {
+                        lp = or;
+                    }
+                }
+
+                if (i < M - 1 && re[i + 1] == '*')
+                {
+                    G.AddEdge(lp,i+1);
+                    G.AddEdge(i+1,lp);
+                }
+
+                if (re[i] == '(' || re[i] == '*' || re[i] == ')')
+                {
+                    G.AddEdge(i,i+1);
+                }
+            }
+        }
+
+        public bool Recognizes(string txt)
+        {
+            var pc = new HashSet<int>();
+            var dfs = new DirectedDfs(G, 0);
+
+            for (int v = 0; v < G.V(); v++)
+            {
+                if (dfs.IsMarked(v))
+                {
+                    pc.Add(v);
+                }
+            }
+
+
+            foreach (var t in txt)
+            {
+                var match = new HashSet<int>();
+
+                foreach (var v in pc)
+                {
+                    if (v < M)
+                    {
+                        if (re[v] == t || re[v] == '.')
+                        {
+                            match.Add(v + 1);
+                        }
+                    }
+                }
+
+                pc = new HashSet<int>();
+                dfs = new DirectedDfs(G, match);
+
+                for (var v = 0; v < G.V(); v++)
+                {
+                    if (dfs.IsMarked(v))
+                    {
+                        pc.Add(v);
+                    }
+                }
+            }
+
+            return pc.Any(v => v == M);
+        }
+    }
+}
---
+++ algorithmsCSharpTests/RegularExpressions/NFATests.cs
@@ -0,0 +1,27 @@
+﻿using AlgorithmsCSharp.RegularExpressions;
+
+namespace algorithmsCSharpTests.RegularExpressions
+{
+    [TestClass]
+    public class NFATests
+    {
+        [TestMethod]
+        public void TestNFABuild()
+        {
+            var regexp = "(.*AB((C|D*E)F)*G)";
+            var nfa =new Nfa(regexp);
+
+
+        }
+
+
+        [TestMethod]
+        public void TestNFARecognizes()
+        {
+            var regexp = "(.*AB((C|D*E)F)*G)";
+            var nfa = new Nfa(regexp);
+            var result = nfa.Recognizes("CCABDEFG");
+            Assert.IsTrue(result);
+        }
+    }
+}
