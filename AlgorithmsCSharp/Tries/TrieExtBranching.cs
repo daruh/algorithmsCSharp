7eea64f5f1d95ea6dd800a0f47003c263ff1c5af
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-08-23 12:49:21 +0200

    Tries External Branching: start put implementation

---
+++ AlgorithmsCSharp/Tries/TrieExtBranching.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCSharp.Tries
+{
+    public class TrieExtBranching<Value>
+    {
+        protected static int R = 256;
+
+        private Node<Value> _root;
+
+        private class Node<Value>
+        {
+            public Value? Val { get; set; }
+
+            public Node<Value>[] Next = new Node<Value>[R];
+
+            public int Size { get; set; }
+
+            public string Characters { get; set; }
+        }
+
+
+        public void Put(string key, Value val)
+        {
+            _root = put(_root, key, val, 0, true);
+        }
+
+        private Node<Value> put(Node<Value> x, string key, Value val, int digit, bool isNewKey)
+        {
+            if (x == null)
+            {
+                x = new Node<Value>()
+                {
+                    Val = val,
+                    Size = 1
+                };
+
+                if (digit != key.Length)
+                {
+                    x.Characters = key.Substring(digit, key.Length);
+                }
+
+                return x;
+            }
+
+            if (isNewKey)
+            {
+                x.Size++;
+            }
+
+            if (x.Characters != null)
+            {
+                var nodeCharactersLength = x.Characters.Length;
+
+                //TODO if path exists only fill Val
+
+                var parentNode = new Node<Value>();
+                parentNode.Size = 2;
+
+                var currentNode = parentNode;
+                var maxLength = Math.Max(nodeCharactersLength, key.Length - digit);
+
+                
+
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ algorithmsCSharpTests/Tries/TrieExtBranchingTests.cs
@@ -0,0 +1,26 @@
+﻿using AlgorithmsCSharp.Tries;
+
+namespace algorithmsCSharpTests.Tries
+{
+    [TestClass]
+    public class TrieExtBranchingTests
+    {
+        [TestMethod]
+        public void TestTrieBuild()
+        {
+            var listOfWords = new List<Tuple<string, int>>()
+            {
+                new("shells", 1),
+                new("shellfish", 2),
+            };
+
+            var trie = new TrieExtBranching<object>();
+
+            foreach (var val in listOfWords)
+            {
+                trie.Put(val.Item1, val.Item2);
+            }
+            
+        }
+    }
+}
